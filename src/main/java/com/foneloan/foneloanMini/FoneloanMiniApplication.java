7b324b1439bd1c6bc48aa156767ae9faa7321ff1
saksham11kc <96900198+saksham11kc@users.noreply.github.com>
2025-04-09 17:51:47 +0545

    First Commit

---
+++ src/main/java/com/foneloan/foneloanMini/FoneloanMiniApplication.java
@@ -0,0 +1,13 @@
+package com.foneloan.foneloanMini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoneloanMiniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoneloanMiniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/foneloan/foneloanMini/FoneloanMiniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foneloan.foneloanMini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoneloanMiniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
